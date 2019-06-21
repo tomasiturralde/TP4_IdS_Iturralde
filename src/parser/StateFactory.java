1f70a73e15845766691aafeaf6595c6397df7f45
tomasiturralde <tomas.iturralde@ing.austral.edu.ar>
2019-06-21 12:34:16 -0300

    stateFactory now works as intended

--- src/parser/StateFactory.java
+++ src/parser/StateFactory.java
@@ -1,23 +1,39 @@
 package parser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StateFactory {
     private State currentState;
-    private State startingState;
+    private List<State> states;
 
     public StateFactory() {
         currentState = new State(false);
-        startingState = currentState;
+        states = new ArrayList<>();
+        states.add(currentState);
     }
 
     public State getStartingState() {
-        return startingState;
+        return states.get(0);
     }
 
     public void createStates(String acceptedLine) {
         String[] transitions = acceptedLine.split("->");
         for (String s : transitions) {
 
-            //TODO: this doesn't work, needs to divide by routes ([route])
+            //if it's a number, go back to the number's position
+            if (s.matches("[0-9]+")) {
+                currentState = states.get(Integer.parseInt(s));
+                continue;
+            }
+
+            //if it's an add number, add that state to the current one
+            if (s.matches("A [0-9]+ [a-zA-Z]+ ?[a-zA-Z]*")) {
+                createExtraTransition(s);
+                continue;
+            }
+
+            //Create a new state and go to that one
             String[] transition = s.split(" ");
             if (transition.length == 2) {
                 createAState(transition[0], transition[1]);
@@ -25,10 +41,20 @@ public class StateFactory {
                 createAState(transition[0], ".*");
             }
 
-
-
         }
-        currentState = startingState;
+        currentState = getStartingState();
+    }
+
+    private void createExtraTransition(String transition) {
+        String[] stateId = transition.split(" ");
+        State stateToAdd = states.get(Integer.parseInt(stateId[1]));
+        Transition newTransition;
+        if (stateId.length == 3) {
+            newTransition = new Transition(stateId[2], ".*", stateToAdd);
+        } else {
+            newTransition = new Transition(stateId[2], stateId[3], stateToAdd);
+        }
+        currentState.setTransition(newTransition);
     }
 
     private void createAState(String type, String text) {
@@ -37,6 +63,7 @@ public class StateFactory {
 
         Transition transition = new Transition(type, text, newState);
         currentState.setTransition(transition);
+        states.add(newState);
         currentState = newState;
     }
 }
